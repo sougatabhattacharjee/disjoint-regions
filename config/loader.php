f1ad24b19f2811b74bed6b5af661036a1626e3ca
Basia Horwath <basia.horwath@gmail.com>
2015-08-08 18:19:50 +0100

    Changed directory structure

--- config/loader.php
+++ config/loader.php
@@ -1,4 +1,4 @@
 <?php
 
-require_once(realpath(dirname(__FILE__) . '/../Model/RegionFinder.php'));
-require_once(realpath(dirname(__FILE__) . '/../Tests/MatrixProvider.php'));
+require_once(realpath(dirname(__FILE__) . '/../src/Model/RegionFinder.php'));
+require_once(realpath(dirname(__FILE__) . '/../src/Tests/MatrixProvider.php'));
